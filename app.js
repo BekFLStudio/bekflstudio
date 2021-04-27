54b235bdc50798ef74f5674dee24aacd1585358f
Izzatulla321 <82449069+Izzatulla321@users.noreply.github.com>
2021-04-27 19:47:08 +0500

    Yangi

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var musicRouter = require('./routes/music');
+var directorRouter = require('./routes/director');
+
+// mongodbga onlayn ulandik 
+
+const db = require('./helper/db')
+db()
+
 
 var app = express();
 
@@ -15,12 +23,14 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/music', musicRouter);
+app.use('/director', directorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Users = require('../Model/Users');
+const bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', function(req, res, next) {
+
+  const {username, password} = req.body;
+
+  // const user = new Users({
+  //   username,
+  //   password,
+  // })
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    const user = new Users({
+      username,
+      password: hash
+    })
+    
+  })
+
+const promise = user.save();
+promise.then(data => res.json(data))
+.catch(err => console.log(err));
+
+})
+
 module.exports = router;
+
